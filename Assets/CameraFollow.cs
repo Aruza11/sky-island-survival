aab87c55e87f7fe3da11298abb8a5e0b6ba6611e
Caroline <carolinewang01@gmail.com>
2020-05-21 17:54:27 -0400

    initially adding of unity game files

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed = 0.125f; //Higher value, faster camera locks onto target, needs to be between 0 and 1
+    public Vector3 offset;
+
+    void LateUpdate ()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float move_speed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // Better for handling inputs
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");    // You can find these names in 
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    // An update that is independent of the frame rate, called on a fixed timer
+    // More reliable for physics and movement
+    void FixedUpdate()
+    {
+        // Should we normalize the movement vector?
+        float m = movement.magnitude;
+        if (m > 0)
+        {
+            rb.MovePosition(rb.position + (movement / m) * move_speed * Time.fixedDeltaTime);
+        }
+    }
+}
