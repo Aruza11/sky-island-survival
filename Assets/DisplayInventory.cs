03f0efdc8a59b4a2d8b72f14c58fe92c3a6cfbb5
Caroline <carolinewang01@gmail.com>
2020-05-30 13:25:45 -0400

    finished inventory v1; inventory is pegged to camera, item counts auto-update

--- Assets/DisplayInventory.cs
+++ Assets/DisplayInventory.cs
@@ -7,6 +7,8 @@ using System;
 public class DisplayInventory : MonoBehaviour
 {
     public InventoryObject inventory;
+    public int X_START;
+    public int Y_START; 
     public int X_SPACE_BETWEEN_ITEM;
     public int NUMBER_OF_COLUMN;
     public int Y_SPACE_BETWEEN_ITEMS;
@@ -30,17 +32,33 @@ public class DisplayInventory : MonoBehaviour
             var obj = Instantiate(inventory.Container[i].item.prefab, Vector2.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            itemsDisplayed.Add(inventory.Container[i], obj);
+
         }
     }
 
     public Vector2 GetPosition(int i)
     {
-        return new Vector2(X_SPACE_BETWEEN_ITEM * (i% NUMBER_OF_COLUMN), 
-                          (-Y_SPACE_BETWEEN_ITEMS * (i/NUMBER_OF_COLUMN)));
+        return new Vector2(X_START + (X_SPACE_BETWEEN_ITEM * (i% NUMBER_OF_COLUMN)), 
+                          Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i/NUMBER_OF_COLUMN)));
 
     }
 
     public void UpdateDisplay()
     {
+        for (int i=0; i < inventory.Container.Count; i++)
+        {
+            if (itemsDisplayed.ContainsKey(inventory.Container[i]))
+            {
+                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            }
+            else
+            {
+                var obj = Instantiate(inventory.Container[i].item.prefab, Vector2.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+                itemsDisplayed.Add(inventory.Container[i], obj);
+            }
+        }
     }
 }
