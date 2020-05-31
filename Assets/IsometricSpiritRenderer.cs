551914e5fd2f14f0887b16187bd2f5c56de9695b
Ben Nativi <bmn8@duke.edu>
2020-05-31 17:23:34 -0400

    Implemented isometric rendering so that objects are sorted correctly (player can run in front or behind obstacles

---
+++ Assets/IsometricSpiritRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class IsometricSpiritRenderer : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Renderer>().sortingOrder = (int)(transform.position.y * -10);
+    }
+}
